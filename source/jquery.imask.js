76e85a7feb8845847024dee8c8972cf48b2a096a
Shawn Wilson <swilson@acs.on.ca>
2011-12-29 16:51:40 -0500

    Added shift keys

--- source/jquery.imask.js
+++ source/jquery.imask.js
@@ -1,3 +1,237 @@
+/******
+ *  This is a function that returns character or string that corresponds
+ *  to the keycode pressed.  
+ *  
+ *  @param {integer} key
+ *    This is the keycode returned by a keystroke event
+ *  @param {boolean} shift
+ *    If true, this will return characters that would result if the
+ *    shift key was held down while the key was pressed.
+ *  
+ ******/
+chrFromKey: function(key, shift) {
+   var chr = ''
+     , keys = {
+            8 : "backspace"
+        ,   9 : "tab"
+        ,  13 : "enter"
+        ,  16 : "shift"
+        ,  17 : "ctrl"
+        ,  18 : "alt"
+        ,  19 : "pause/break"
+        ,  20 : "caps-lock"
+        ,  27 : "escape"
+        ,  33 : "page-up"
+        ,  34 : "page-down"
+        ,  35 : "end"
+        ,  36 : "home"
+        ,  37 : "left-arrow"
+        ,  38 : "up-arrow"
+        ,  39 : "right-arrow"
+        ,  40 : "down-arrow"
+        ,  45 : "insert"
+        ,  46 : "delete"
+        ,  48 : "0"
+        ,  49 : "1"
+        ,  50 : "2"
+        ,  51 : "3"
+        ,  52 : "4"
+        ,  53 : "5"
+        ,  54 : "6"
+        ,  55 : "7"
+        ,  56 : "8"
+        ,  57 : "9"
+        ,  65 : "a"
+        ,  66 : "b"
+        ,  67 : "c"
+        ,  68 : "d"
+        ,  69 : "e"
+        ,  70 : "f"
+        ,  71 : "g"
+        ,  72 : "h"
+        ,  73 : "i"
+        ,  74 : "j"
+        ,  75 : "k"
+        ,  76 : "l"
+        ,  77 : "m"
+        ,  78 : "n"
+        ,  79 : "o"
+        ,  80 : "p"
+        ,  81 : "q"
+        ,  82 : "r"
+        ,  83 : "s"
+        ,  84 : "t"
+        ,  85 : "u"
+        ,  86 : "v"
+        ,  87 : "w"
+        ,  88 : "x"
+        ,  89 : "y"
+        ,  90 : "z"
+        ,  91 : "left-window-key"
+        ,  92 : "right-window-key"
+        ,  93 : "select-key"
+        ,  96 : "numpad-0"
+        ,  97 : "numpad-1"
+        ,  98 : "numpad-2"
+        ,  99 : "numpad-3"
+        , 100 : "numpad-4"
+        , 101 : "numpad-5"
+        , 102 : "numpad-6"
+        , 103 : "numpad-7"
+        , 104 : "numpad-8"
+        , 105 : "numpad-9"
+        , 106 : "multiply"
+        , 107 : "add"
+        , 109 : "subtract"
+        , 110 : "decimal-point"
+        , 111 : "divide"
+        , 112 : "f1"
+        , 113 : "f2"
+        , 114 : "f3"
+        , 115 : "f4"
+        , 116 : "f5"
+        , 117 : "f6"
+        , 118 : "f7"
+        , 119 : "f8"
+        , 120 : "f9"
+        , 121 : "f10"
+        , 122 : "f11"
+        , 123 : "f12"
+        , 144 : "num-lock"
+        , 145 : "scroll-lock"
+        , 186 : "semi-colon"
+        , 187 : "equal-sign"
+        , 188 : "comma"
+        , 189 : "dash"
+        , 190 : "period"
+        , 191 : "forward-slash"
+        , 192 : "grave-accent"
+        , 219 : "open-bracket"
+        , 220 : "back-slash"
+        , 221 : "close-braket"
+        , 222 : "single-quote"
+       } 
+     , shiftkeys = {
+            8 : "backspace"
+        ,   9 : "tab"
+        ,  13 : "enter"
+        ,  16 : "shift"
+        ,  17 : "shift-ctrl"
+        ,  18 : "shift-alt"
+        ,  19 : "pause/break"
+        ,  20 : "caps-lock"
+        ,  27 : "escape"
+        ,  33 : "page-up"
+        ,  34 : "page-down"
+        ,  35 : "end"
+        ,  36 : "home"
+        ,  37 : "left-arrow"
+        ,  38 : "up-arrow"
+        ,  39 : "right-arrow"
+        ,  40 : "down-arrow"
+        ,  45 : "insert"
+        ,  46 : "delete"
+        ,  48 : ")"
+        ,  49 : "!"
+        ,  50 : "@"
+        ,  51 : "#"
+        ,  52 : "$"
+        ,  53 : "%"
+        ,  54 : "^"
+        ,  55 : "&"
+        ,  56 : "*"
+        ,  57 : "("
+        ,  65 : "A"
+        ,  66 : "B"
+        ,  67 : "C"
+        ,  68 : "D"
+        ,  69 : "E"
+        ,  70 : "F"
+        ,  71 : "G"
+        ,  72 : "H"
+        ,  73 : "I"
+        ,  74 : "J"
+        ,  75 : "K"
+        ,  76 : "L"
+        ,  77 : "M"
+        ,  78 : "N"
+        ,  79 : "O"
+        ,  80 : "P"
+        ,  81 : "Q"
+        ,  82 : "R"
+        ,  83 : "S"
+        ,  84 : "T"
+        ,  85 : "U"
+        ,  86 : "V"
+        ,  87 : "W"
+        ,  88 : "X"
+        ,  89 : "Y"
+        ,  90 : "Z"
+        ,  91 : "shift-left-window-key"
+        ,  92 : "shift-right-window-key"
+        ,  93 : "select-key"
+        ,  96 : "numpad-0"
+        ,  97 : "numpad-1"
+        ,  98 : "numpad-2"
+        ,  99 : "numpad-3"
+        , 100 : "numpad-4"
+        , 101 : "numpad-5"
+        , 102 : "numpad-6"
+        , 103 : "numpad-7"
+        , 104 : "numpad-8"
+        , 105 : "numpad-9"
+        , 106 : "multiply"
+        , 107 : "+"
+        , 109 : "-"
+        , 110 : "."
+        , 111 : "/"
+        , 112 : "shift-f1"
+        , 113 : "shift-f2"
+        , 114 : "shift-f3"
+        , 115 : "shift-f4"
+        , 116 : "shift-f5"
+        , 117 : "shift-f6"
+        , 118 : "shift-f7"
+        , 119 : "shift-f8"
+        , 120 : "shift-f9"
+        , 121 : "shift-f10"
+        , 122 : "shift-f11"
+        , 123 : "shift-f12"
+        , 144 : "num-lock"
+        , 145 : "scroll-lock"
+        , 186 : ":"
+        , 187 : "+"
+        , 188 : "<"
+        , 189 : "_"
+        , 190 : ">"
+        , 191 : "?"
+        , 192 : "grave-accent"
+        , 219 : "{"
+        , 220 : "|"
+        , 221 : "}"
+        , 222 : '"'
+       };
+
+   console.log("key=", key, keys[key]); 
+
+   /* Shift number-pad numbers to corresponding character codes. */
+   if (key >= 96 && key <= 105) { 
+      key -= 48; 
+
+   /* MINUS SIGN */
+   } else if (key == 109 || key == 189 || key == 45) {
+      return '-';
+
+   /* PERIOD */
+   } else if (key == 190 || key == 46) {
+      return '.';
+   } // endif
+
+   return keys[key];
+}, // endfunction
+
+
+
 (function($){
    var $chk = function(obj){
       return !!(obj || obj === 0);
@@ -844,134 +1078,6 @@
          } // endif
 
          return output;
-      }, // endfunction
-
-
-
-      /******
-       *  Convert the event keycode from a number to a character.
-       *  All upper case keycodes get converted to lower case.
-       ******/
-      chrFromEv: function(ev) {
-         var chr = ''
-           , key = ev.which
-           , keys = {
-                  8 : "backspace"
-              ,   9 : "tab"
-              ,  13 : "enter"
-              ,  16 : "shift"
-              ,  17 : "ctrl"
-              ,  18 : "alt"
-              ,  19 : "pause/break"
-              ,  20 : "caps-lock"
-              ,  27 : "escape"
-              ,  33 : "page-up"
-              ,  34 : "page-down"
-              ,  35 : "end"
-              ,  36 : "home"
-              ,  37 : "left-arrow"
-              ,  38 : "up-arrow"
-              ,  39 : "right-arrow"
-              ,  40 : "down-arrow"
-              ,  45 : "insert"
-              ,  46 : "delete"
-              ,  48 : "0"
-              ,  49 : "1"
-              ,  50 : "2"
-              ,  51 : "3"
-              ,  52 : "4"
-              ,  53 : "5"
-              ,  54 : "6"
-              ,  55 : "7"
-              ,  56 : "8"
-              ,  57 : "9"
-              ,  65 : "a"
-              ,  66 : "b"
-              ,  67 : "c"
-              ,  68 : "d"
-              ,  69 : "e"
-              ,  70 : "f"
-              ,  71 : "g"
-              ,  72 : "h"
-              ,  73 : "i"
-              ,  74 : "j"
-              ,  75 : "k"
-              ,  76 : "l"
-              ,  77 : "m"
-              ,  78 : "n"
-              ,  79 : "o"
-              ,  80 : "p"
-              ,  81 : "q"
-              ,  82 : "r"
-              ,  83 : "s"
-              ,  84 : "t"
-              ,  85 : "u"
-              ,  86 : "v"
-              ,  87 : "w"
-              ,  88 : "x"
-              ,  89 : "y"
-              ,  90 : "z"
-              ,  91 : "left-window-key"
-              ,  92 : "right-window-key"
-              ,  93 : "select-key"
-              ,  96 : "numpad-0"
-              ,  97 : "numpad-1"
-              ,  98 : "numpad-2"
-              ,  99 : "numpad-3"
-              , 100 : "numpad-4"
-              , 101 : "numpad-5"
-              , 102 : "numpad-6"
-              , 103 : "numpad-7"
-              , 104 : "numpad-8"
-              , 105 : "numpad-9"
-              , 106 : "multiply"
-              , 107 : "add"
-              , 109 : "subtract"
-              , 110 : "decimal-point"
-              , 111 : "divide"
-              , 112 : "f1"
-              , 113 : "f2"
-              , 114 : "f3"
-              , 115 : "f4"
-              , 116 : "f5"
-              , 117 : "f6"
-              , 118 : "f7"
-              , 119 : "f8"
-              , 120 : "f9"
-              , 121 : "f10"
-              , 122 : "f11"
-              , 123 : "f12"
-              , 144 : "num-lock"
-              , 145 : "scroll-lock"
-              , 186 : "semi-colon"
-              , 187 : "equal-sign"
-              , 188 : "comma"
-              , 189 : "dash"
-              , 190 : "period"
-              , 191 : "forward-slash"
-              , 192 : "grave-accent"
-              , 219 : "open-bracket"
-              , 220 : "back-slash"
-              , 221 : "close-braket"
-              , 222 : "single-quote"
-             };
-
-         console.log("key=", key, keys[key]); 
-   
-         /* Shift number-pad numbers to corresponding character codes. */
-         if (key >= 96 && key <= 105) { 
-            key -= 48; 
-
-         /* MINUS SIGN */
-         } else if (key == 109 || key == 189 || key == 45) {
-            return '-';
-
-         /* PERIOD */
-         } else if (key == 190 || key == 46) {
-            return '.';
-         } // endif
-
-         return keys[key];
       }, // endfunction
 
 
