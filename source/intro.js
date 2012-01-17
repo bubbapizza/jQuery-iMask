5fe097d754341770600393d1a22dbfa6af30fb86
Shawn Wilson <swilson@acs.on.ca>
2012-01-17 10:08:40 -0500

    Changed email address

--- source/intro.js
+++ source/intro.js
@@ -4,7 +4,7 @@
  * Licensed under the MIT License
  * Authors: Mark Kahn
  *          Fabio Zendhi Nagao (http://zend.lojcomm.com.br)
- *          Shawn Wilson (shawn@ch2a.ca)
+ *          Shawn Wilson (swilson@acs.on.ca)
  *
  * Date: @DATE
  */
